c5ead7044ca3c97dddad7d1e278e9f1be33e82e3
jamersonsouza <jamersonsouza@outlook.com>
2025-05-16 22:55:18 -0300

    refactor: implementação do user details

--- src/main/java/tech/jamersondev/gratitude/core/model/User.java
+++ src/main/java/tech/jamersondev/gratitude/core/model/User.java
@@ -3,13 +3,18 @@ package tech.jamersondev.gratitude.core.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
-import java.util.UUID;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
-public class User extends CoreEntity {
+public class User extends CoreEntity implements UserDetails {
 
     @Column(length = 128)
     private String email;
@@ -22,31 +27,35 @@ public class User extends CoreEntity {
 
     private Date createdDate;
 
-    public User() {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return  List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
-    public Date getCreatedDate() {
-        return createdDate;
+    @Override
+    public String getUsername() {
+        return getEmail();
     }
 
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public String getEmail() {
@@ -57,5 +66,47 @@ public class User extends CoreEntity {
         this.email = email;
     }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        User user = (User) o;
+        return Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(createdDate, user.createdDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + Objects.hashCode(email);
+        result = 31 * result + Objects.hashCode(password);
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(createdDate);
+        return result;
+    }
 }
--- src/main/java/tech/jamersondev/gratitude/core/repository/UserRepository.java
+++ src/main/java/tech/jamersondev/gratitude/core/repository/UserRepository.java
@@ -3,5 +3,8 @@ package tech.jamersondev.gratitude.core.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.jamersondev.gratitude.core.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String login);
 }
