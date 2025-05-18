eeb7022be1218db94a6e5e84ffe075911a1b15a1
jamersonsouza <jamersonsouza@outlook.com>
2025-05-17 22:50:08 -0300

    fix: correção para liberação das rotas permitidas

--- src/main/java/tech/jamersondev/gratitude/config/SecurityConfiguration.java
+++ src/main/java/tech/jamersondev/gratitude/config/SecurityConfiguration.java
@@ -2,10 +2,12 @@ package tech.jamersondev.gratitude.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,10 +28,11 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
            return http
                     .headers(h -> h.frameOptions(f -> f.sameOrigin()))
-                    .sessionManagement(session ->
+                   .csrf(CsrfConfigurer::disable)
+                   .sessionManagement(session ->
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authorizeHttpRequests(request -> {
-                        request.requestMatchers("/login", "/create-user").permitAll();
+                        request.requestMatchers(HttpMethod.POST,"/login", "/user").permitAll();
                         request.anyRequest().authenticated();
                     })
                     .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
