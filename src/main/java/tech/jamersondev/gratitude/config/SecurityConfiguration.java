5f82fd628e878b6d3b4671fe35c7f202f1ab3100
jamersonsouza <jamersonsouza@outlook.com>
2025-05-27 20:13:06 -0300

    ✨ feat:  implementação e config do swagger pra documentação da api

--- src/main/java/tech/jamersondev/gratitude/config/SecurityConfiguration.java
+++ src/main/java/tech/jamersondev/gratitude/config/SecurityConfiguration.java
@@ -36,6 +36,7 @@ public class SecurityConfiguration {
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authorizeHttpRequests(request -> {
                         request.requestMatchers(HttpMethod.POST,"/login", "/user").permitAll();
+                        request.requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                         request.anyRequest().authenticated();
                     })
                    .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
