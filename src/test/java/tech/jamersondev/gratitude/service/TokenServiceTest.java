0c42907292becb17a7dcc5b2d85a689e52ec79b3
jamersonsouza <jamersonsouza@outlook.com>
2025-05-27 22:44:30 -0300

    🧪 test: refatoração no teste para a camada de serviço de token

--- src/test/java/tech/jamersondev/gratitude/service/TokenServiceTest.java
+++ src/test/java/tech/jamersondev/gratitude/service/TokenServiceTest.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import tech.jamersondev.gratitude.core.model.User;
 import tech.jamersondev.gratitude.core.service.TokenService;
 
