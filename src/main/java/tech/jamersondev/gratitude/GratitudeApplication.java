52e203512836f60e50666b7eb9d96f98165df4b0
jamersonsouza <jamersonsouza@outlook.com>
2025-06-02 20:44:21 -0300

    perf ⚡️: ativando o cacheamento na aplicação

--- src/main/java/tech/jamersondev/gratitude/GratitudeApplication.java
+++ src/main/java/tech/jamersondev/gratitude/GratitudeApplication.java
@@ -2,8 +2,10 @@ package tech.jamersondev.gratitude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GratitudeApplication {
 
 	public static void main(String[] args) {
